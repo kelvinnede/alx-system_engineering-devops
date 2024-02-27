ea71ed5a10bcf6c01c6d6200858d705c5ca11341
Kelvin Nede <kelvinnede@gmail.com>
2024-02-27 02:54:12 -0800

    2-repetition_token_1.rb

---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+# Check if there's an argument provided
+if ARGV.empty?
+  puts "Usage: #{$PROGRAM_NAME} <text>"
+  exit 1
+end
+
+# Get the argument from the command line
+text = ARGV[0]
+
+# Define the regular expression
+regex = /hbt+n/
+
+# Match the regular expression against the input text
+matches = text.scan(regex)
+
+# Print the matched results
+puts matches.join
