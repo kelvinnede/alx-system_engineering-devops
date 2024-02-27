9cf34af97e64225368601a3a2868f0139c54ae7b
Kelvin Nede <kelvinnede@gmail.com>
2024-02-27 13:56:28 -0800

    6-phone_number.rb

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,20 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if there's an argument provided
-if ARGV.empty?
-  puts "Usage: #{$PROGRAM_NAME} <phone_number>"
-  exit 1
-end
-
-# Get the argument from the command line
-phone_number = ARGV[0]
-
-# Define the regular expression
-regex = /\A\d{10}\z/
-
-Match the regular expression against the input phone number
-if phone_number.match?(regex)
-  puts phone_number
-else
-  puts ""
-end
+puts ARGV[0].scan(/^\d{10,10}$/).join
