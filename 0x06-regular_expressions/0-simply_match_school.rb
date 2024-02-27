e5729283ceb8445fc0c41359b6bdae64d3713f88
Kelvin Nede <kelvinnede@gmail.com>
2024-02-27 14:01:19 -0800

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,19 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if there's an argument provided
-if ARGV.empty?
-  puts "Usage: #{$PROGRAM_NAME} <text>"
-  exit 1
-end
-
-# Get the argument from the command line
-text = ARGV[0]
-
-# Define the regular expression
-regex = /School/
-
-# Match the regular expression against the input text
-matches = text.scan(regex)
-
-# Print the matched results
-puts matches.join
+puts ARGV[0].scan(/School/).join
