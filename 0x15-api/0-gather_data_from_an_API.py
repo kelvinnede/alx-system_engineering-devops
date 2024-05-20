a3d15835be07042b08e66398d0e24e587d5fe1f5
kelvinnede <kelvinnede@gmail.com>
2024-05-20 07:16:34 -0700

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,56 +1,29 @@
-#include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
+#!/usr/bin/python3
+"""
+a script that returns info about a customer's todo list
+"""
+import requests
+from sys import argv
 
-/**
-* read_textfile - Reads a text file and prints
-* it to the POSIX standard output.
-* @filename: The name of the file to be read.
-* @letters: The number of letters to read and print.
-*
-* Return: The actual number of letters read and printed. 0 on failure.
-*/
-ssize_t read_textfile(const char *filename, size_t letters)
-{
-        int file_descriptor;
-        char *buffer;
-        ssize_t bytes_read;
-        ssize_t bytes_written;
 
-        if (filename == NULL)
-        return (0);
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com"
+    employeeId = argv[1]
 
-        file_descriptor = open(filename, O_RDONLY);
+    employee = requests.get("{}/users/{}".format(url, employeeId)).json()
+    todos = requests.get(url + "/todos", params={"userId": employeeId}).json()
 
-        if (file_descriptor == -1)
-                return (0);
+    completed_tasks = []
+    for data in todos:
+        if data.get('completed') is True:
+            completed_tasks.append(data.get('title'))
 
-        buffer = malloc(sizeof(char) * letters);
+    employee_name = employee.get('name')
+    total_num_of_tasks = len(todos)
+    num_of_tasks_done = len(completed_tasks)
 
-        if (buffer == NULL)
-        {
-                close(file_descriptor);
-                return (0);
-        }
+    print("Employee {} is done with tasks({}/{}):".format(employee_name,
+          num_of_tasks_done, total_num_of_tasks))
 
-        bytes_read = read(file_descriptor, buffer, letters);
-
-        if (bytes_read == -1)
-        {
-                 free(buffer);
-                 close(file_descriptor);
-                 return (0);
-        }
-
-        bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
-        free(buffer);
-        close(file_descriptor);
-
-        if (bytes_written == -1 || bytes_written != bytes_read)
-                return (0);
-
-        return (bytes_written);
-}
+    for title in completed_tasks:
+        print("\t {}".format(title))
