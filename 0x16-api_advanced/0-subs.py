bf74ea917ff8cc7464387c75711c6c76d7b1f0f7
kelvinnede <kelvinnede@gmail.com>
2024-06-04 07:23:37 -0700

    0-main.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,11 @@
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'My User Agent'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
